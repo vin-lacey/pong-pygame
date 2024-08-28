d7ed031634570b4b85b0bab5270e4ecaacaecaab
Vin Lacey <vin.lacey@aol.com>
2024-08-28 15:44:36 +0100

    added ai and some redesign

--- main.py
+++ main.py
@@ -1,32 +1,71 @@
-import pygame
+from numpy import random
+import pygame, sys
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 500
-FPS = 60
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
 
-running = True
-clock = pygame.time.Clock()
+    if ball.top <= 0 or ball.bottom >= SCREEN_HEIGHT:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= SCREEN_WIDTH:
+        ball.x = SCREEN_WIDTH // 2 - 6
+        ball.y = SCREEN_HEIGHT // 2 - 15
+        pygame.time.wait(2000)
+        ball_speed_x *= -1
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-font = pygame.font.Font("assets/PressStart2P-Regular.ttf", 36)
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+def ai(opponent):
+    # AI logic
+    ai_speed = random.randint(1, 10)
+    if opponent.top < ball.y:
+        opponent.y += ai_speed
+    if opponent.bottom > ball.y:
+        opponent.y -= ai_speed
 
 
 pygame.init()
+clock = pygame.time.Clock()
 
-def main():
-    while running:
-        clock.tick(FPS)
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+FPS = 60
+WHITE = (245, 245, 245)
+BLACK = (50, 50, 50)
 
-        screen.fill((0, 0, 0))
+ball_speed_x = 7
+ball_speed_y = 7
+paddle_speed = 10
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Pong')
 
-        pygame.display.update
+ball = pygame.Rect(SCREEN_WIDTH // 2 - 6, SCREEN_HEIGHT // 2 - 15, 15, 15)
+opponent = pygame.Rect(SCREEN_WIDTH - 20, SCREEN_HEIGHT // 2 - 70, 15, 140)
+player = pygame.Rect(10, SCREEN_HEIGHT // 2 - 70, 15, 140)
 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w] and player.top > 0:
+        player.y -= paddle_speed
+    if key[pygame.K_s] and player.bottom < SCREEN_HEIGHT:
+        player.y += paddle_speed
 
-    pygame.quit()
+    ball_animation()
+    ai(opponent)
 
-if __name__ == "__main__":
-    main()
+    screen.fill(BLACK)
+    pygame.draw.rect(screen, WHITE, player)
+    pygame.draw.rect(screen, WHITE, opponent)
+    pygame.draw.ellipse(screen, WHITE, ball)
+    pygame.draw.aaline(screen, WHITE, (SCREEN_WIDTH // 2, 0), (SCREEN_WIDTH // 2, SCREEN_HEIGHT))
+
+    pygame.display.flip()
+    clock.tick(FPS)
